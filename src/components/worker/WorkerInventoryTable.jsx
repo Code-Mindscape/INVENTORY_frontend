a28afa2a7548d4e20df186e1539a1805fd6deabb
USMAN <mirzausmanmu07842@gmail.com>
2025-03-06 19:42:46 +0500

    Change

--- src/components/worker/WorkerInventoryTable.jsx
+++ src/components/worker/WorkerInventoryTable.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import AddProduct from "../pages/AddProduct";
 
 const WorkerInventoryTable = () => {
   const [products, setProducts] = useState([]);
