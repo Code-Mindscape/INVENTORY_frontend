2cbc2d68ae1ef20058eeb36b1ebd5807bbfd1eb7
USMAN <mirzausmanmu07842@gmail.com>
2025-03-06 13:00:25 +0500

    Changed Table to Cards

--- src/components/OrdersTable.jsx
+++ src/components/OrdersTable.jsx
@@ -35,7 +35,7 @@ const OrdersTable = () => {
   }, [currentPage]);
 
   return (
-    <div className="p-4">
+    <div className="p-4 mt-16">
       {loading ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {[...Array(8)].map((_, i) => (
